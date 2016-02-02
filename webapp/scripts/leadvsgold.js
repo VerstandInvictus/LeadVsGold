5a8d61df05f1bacacc7a971447af7cdc801341d9
verstandinvictus <peter.k.cawley@gmail.com>
2016-02-02 10:14:04 -0800

    ACTUALLY make autohost work on port 80

--- webapp/scripts/leadvsgold.js
+++ webapp/scripts/leadvsgold.js
@@ -1,13 +1,17 @@
+var flaskServ = 'http' + window.location.origin.split(':')[1] + '5000'
+
+$('#mainImg').attr('src', + flaskServ + '/image/0')
+
 $(function(){
   $( ".ui-page" ).swipe( {
         swipeLeft:function(event, direction, distance, duration, fingerCount) {
             var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', 'http' + window.location.origin.split(':')[1] +':5000/next/skip/' +
+            $('#mainImg').attr('src', flaskServ + '/next/skip/' +
             numRand);
         },
         swipeRight:function(event, direction, distance, duration, fingerCount) {
             var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', 'http' + window.location.origin.split(':')[1] +':5000/prev/' +
+            $('#mainImg').attr('src', + flaskServ + '/prev/' +
             numRand);
         }
   });
