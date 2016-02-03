f64762fe5cee4b88b1354a53247f4038eac0a141
VerstandInvictus <peter.k.cawley@gmail.com>
2016-02-02 20:56:25 -0800

    add up and down functions

--- webapp/scripts/leadvsgold.js
+++ webapp/scripts/leadvsgold.js
@@ -9,6 +9,16 @@ $(function(){
             $('#mainImg').attr('src', flaskServ + '/next/skip/' +
             numRand);
         },
+        swipeUp:function(event, direction, distance, duration, fingerCount) {
+            var numRand = Math.floor(Math.random() * 10001);
+            $('#mainImg').attr('src', flaskServ + '/next/up/' +
+            numRand);
+        },
+        swipeDown:function(event, direction, distance, duration, fingerCount) {
+            var numRand = Math.floor(Math.random() * 10001);
+            $('#mainImg').attr('src', flaskServ + '/next/down/' +
+            numRand);
+        },
         swipeRight:function(event, direction, distance, duration, fingerCount) {
             var numRand = Math.floor(Math.random() * 10001);
             $('#mainImg').attr('src', flaskServ + '/prev/' +
