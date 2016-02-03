78c55d557067acf95f5876d69264656c4e090501
verstandinvictus <peter.k.cawley@gmail.com>
2016-02-03 13:35:24 -0800

    refactor for increased DRY

--- webapp/scripts/leadvsgold.js
+++ webapp/scripts/leadvsgold.js
@@ -4,55 +4,32 @@ $(".infobox").fadeTo(0,0);
 
 $('#mainImg').attr('src', flaskServ + '/image/0')
 
+function changeImage(actionUrl, infoText, infoColor) {
+    var numRand = Math.floor(Math.random() * 10001);
+    $('.infobox').html('<p>' + infoText + '</p>');
+    $('.infobox').css('background-color', infoColor);
+    $('.infobox').fadeTo(250, 0.7).delay(1000).fadeTo(250, 0);
+    $('#mainImg').attr('src', flaskServ + actionUrl +
+            numRand);
+    $('#mainImg').fadeTo(250, 0);
+    $('#mainImg').load(function() {
+        $('#mainImg').fadeTo(250, 1);
+    });
+}
+
 $(function(){
   $( ".ui-page" ).swipe( {
         swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', flaskServ + '/next/skip/' +
-                    numRand);
-            $('.infobox').html('<p>skipped</p>');
-            $('.infobox').css('background-color', 'grey');
-            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
-            $('#mainImg').fadeTo(250, 0.3);
-            $('#mainImg').load(function() {
-                $('#mainImg').fadeTo(250, 1);
-            });
+            changeImage('/next/skip/', 'skipped', 'grey');
         },
         swipeUp:function(event, direction, distance, duration, fingerCount) {
-            var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', flaskServ + '/next/up/' +
-            numRand);
-            $('.infobox').html('<p>' + upfolder + '</p>');
-            $('.infobox').css('background-color', upcolor);
-            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
-            $('#mainImg').fadeTo(250, 0.3);
-            $('#mainImg').load(function() {
-                $('#mainImg').fadeTo(250, 1);
-            });
+            changeImage('/next/up/', upfolder, upcolor);
         },
         swipeDown:function(event, direction, distance, duration, fingerCount) {
-            var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', flaskServ + '/next/down/' +
-            numRand);
-            $('.infobox').html('<p>' + downfolder + '</p>');
-            $('.infobox').css('background-color', downcolor);
-            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
-            $('#mainImg').fadeTo(250, 0.3);
-            $('#mainImg').load(function() {
-                $('#mainImg').fadeTo(250, 1);
-            });
+            changeImage('/next/down/', downfolder, downcolor);
         },
         swipeRight:function(event, direction, distance, duration, fingerCount) {
-            var numRand = Math.floor(Math.random() * 10001);
-            $('#mainImg').attr('src', flaskServ + '/prev/' +
-            numRand);
-            $('.infobox').html('<p>back</p>');
-            $('.infobox').css('background-color', 'gray');
-            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
-            $('#mainImg').fadeTo(250, 0.3);
-            $('#mainImg').load(function() {
-                $('#mainImg').fadeTo(250, 1);
-            });
+            changeImage('/prev/', 'back', 'grey');
         }
   });
 })
