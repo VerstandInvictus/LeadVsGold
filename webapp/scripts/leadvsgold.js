d94fa697404c0214204066401bd337c08fb608bb
verstandinvictus <peter.k.cawley@gmail.com>
2016-02-03 13:16:22 -0800

    first pass at action infobox fadeover

--- webapp/scripts/leadvsgold.js
+++ webapp/scripts/leadvsgold.js
@@ -1,4 +1,6 @@
-var flaskServ = 'http:' + window.location.origin.split(':')[1] + ':5000'
+var flaskServ = 'http:' + window.location.origin.split(':')[1] + ':5000';
+
+$(".infobox").fadeTo(0,0);
 
 $('#mainImg').attr('src', flaskServ + '/image/0')
 
@@ -8,6 +10,9 @@ $(function(){
             var numRand = Math.floor(Math.random() * 10001);
             $('#mainImg').attr('src', flaskServ + '/next/skip/' +
                     numRand);
+            $('.infobox').html('<p>skipped</p>');
+            $('.infobox').css('background-color', 'grey');
+            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
             $('#mainImg').fadeTo(250, 0.3);
             $('#mainImg').load(function() {
                 $('#mainImg').fadeTo(250, 1);
@@ -17,6 +22,9 @@ $(function(){
             var numRand = Math.floor(Math.random() * 10001);
             $('#mainImg').attr('src', flaskServ + '/next/up/' +
             numRand);
+            $('.infobox').html('<p>' + upfolder + '</p>');
+            $('.infobox').css('background-color', upcolor);
+            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
             $('#mainImg').fadeTo(250, 0.3);
             $('#mainImg').load(function() {
                 $('#mainImg').fadeTo(250, 1);
@@ -26,6 +34,9 @@ $(function(){
             var numRand = Math.floor(Math.random() * 10001);
             $('#mainImg').attr('src', flaskServ + '/next/down/' +
             numRand);
+            $('.infobox').html('<p>' + downfolder + '</p>');
+            $('.infobox').css('background-color', downcolor);
+            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
             $('#mainImg').fadeTo(250, 0.3);
             $('#mainImg').load(function() {
                 $('#mainImg').fadeTo(250, 1);
@@ -35,6 +46,9 @@ $(function(){
             var numRand = Math.floor(Math.random() * 10001);
             $('#mainImg').attr('src', flaskServ + '/prev/' +
             numRand);
+            $('.infobox').html('<p>back</p>');
+            $('.infobox').css('background-color', 'gray');
+            $('.infobox').fadeTo(250, 0.7).delay(750).fadeTo(250, 0);
             $('#mainImg').fadeTo(250, 0.3);
             $('#mainImg').load(function() {
                 $('#mainImg').fadeTo(250, 1);
