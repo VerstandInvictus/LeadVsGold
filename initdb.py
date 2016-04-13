82d75c6b168b4c53beb61ac89d51c04f106228e2
Peter K. Cawley <peter.k.cawley@gmail.com>
2016-04-13 11:15:54 -0700

    and this is why I don't edit directly on github

--- initdb.py
+++ initdb.py
@@ -54,7 +54,7 @@ indexes = dict(
     _id="index",
     batch=1,
     session=1,)
-for folder in initDict['actions'].iteralues():
+for folder in initDict['actions'].itervalues():
     if not os.path.exists(folder):
         os.makedirs(folder)
 fl.drop()
