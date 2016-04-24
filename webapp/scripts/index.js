829b7e68b59f07ce092660058443c188210a1c42
VerstandInvictus <peter.k.cawley@gmail.com>
2016-04-23 18:36:49 -0700

    man do I suck.

--- webapp/scripts/index.js
+++ webapp/scripts/index.js
@@ -1,7 +1,7 @@
 var colors = ['#104060','#125b94','#1f7fc1','#67add8']
 var flaskServ = 'http:' + window.location.origin.split(':')[1] + ':5000';
 
-$get(flaskServ + '/folders', function(data) {
+$.get(flaskServ + '/folders', function(data) {
     $.each(data, function (index, value) {
         var string = '<p class="submit purple"><a href="folders/' +
         value + '">' + value + '</a></p><br>';
