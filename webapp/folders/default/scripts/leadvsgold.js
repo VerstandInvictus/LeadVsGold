77a2052e20e431e4182034bc5fe7689b57c88c26
VerstandInvictus <peter.k.cawley@gmail.com>
2016-09-20 06:34:21 -0700

    fix broken tap function keymaps

--- webapp/folders/default/scripts/leadvsgold.js
+++ webapp/folders/default/scripts/leadvsgold.js
@@ -118,6 +118,7 @@ $( document ).ready(function() {
                 $('#tapbox').html('<p>' + tapfolder + '</p>');
                 $('#tapbox').css('background-color', tapcolor);
                 $('#tapbox').fadeTo(250, 0.7).delay(1000).fadeTo(250, 0);
+                break;
             default:
                 keylog(e.which);
                 return;
